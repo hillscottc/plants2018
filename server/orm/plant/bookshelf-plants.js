82b60ad4352a16cdf187d346c3b7a8f441c44353
Scott Hill <scott289@gmail.com>
2018-05-12 22:09:13 -0700

    more test connecting

--- server/orm/plant/bookshelf-plants.js
+++ server/orm/plant/bookshelf-plants.js
@@ -7,8 +7,24 @@ const Bookshelf = bookshelf(
     client: 'pg',
 
     /*  FIX HARD-CODED DB   */
-    connection: 'postgres://plants_user:password@db/plants_db',
+    // connection: 'postgres://plants_user:password@db/plants_db',
 
+    // psql postgres://plants_user:password@localhost:15432/plants_db
+
+    // connection: {
+    //   host: process.DB_HOST,
+    //   port: process.env.DB_PORT || 5432,
+    //   user: process.env.DB_USER || 'postgres',
+    //   password: process.env.DB_PASSWORD,
+    //   database: process.env.DB_NAME || 'postgres'
+    // },
+    connection: {
+      host: 'localhost',
+      port: 15432,
+      user: 'plants_user',
+      password: 'password',
+      database: 'plants_db'
+    },
     pool: {
       min: 0,
       max: 10,
