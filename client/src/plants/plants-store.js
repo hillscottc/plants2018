5dfeb92707b83632d6a11c8ff6f6b423a4ff6a0f
Scott Hill <scott289@gmail.com>
2018-05-13 19:39:56 -0700

    having conection problems

--- client/src/plants/plants-store.js
+++ client/src/plants/plants-store.js
@@ -1,12 +1,21 @@
-export async function searchPlants ({common, family, symbol, sci, limit = 10, offset = 0}) {
+// import querystring from 'querystring'
+// import { checkHttpResp } from '../utils.js'
+
+// const API_HOST = process.env.REACT_APP_API_HOST || 'https://sch-datahub.herokuapp.com'
+
+export function searchPlants ({common, family, symbol, sci, limit = 10, offset = 0}) {
+  const payload = {common, family, symbol, sci, limit, offset}
+
   const fetchArgs = {
     headers: {'Accept': 'application/json', 'Content-Type': 'application/json'},
     method: 'POST',
-    body: JSON.stringify({common, family, symbol, sci, limit, offset})
+    body: JSON.stringify(payload)
   }
-  const response = await fetch('/api/plants/', fetchArgs) // eslint-disable-line no-undef
-  const body = await response.json()
-  if (response.status !== 200) throw Error(body.message)
-  const {data, pagination} = body
-  return { data, pagination }
+  return fetch('/api/plants/', fetchArgs) // eslint-disable-line no-undef
+    // .then(checkHttpResp)
+    .then((response) => response.json())
+    .then((json) => {
+      const {data, pagination} = json
+      return { data, pagination }
+    })
 }
--- server/orm/plant/bookshelf-plants.js
+++ server/orm/plant/bookshelf-plants.js
@@ -6,20 +6,23 @@ const Bookshelf = bookshelf(
   knex({
     client: 'pg',
 
-    // connection: {
-    //   host: process.DB_HOST,
-    //   port: process.env.DB_PORT || 15432,
-    //   user: process.env.DB_USER || 'plants_user',
-    //   password: process.env.DB_PASSWORD || 'password',
-    //   database: process.env.DB_NAME || 'plants_db'
-    // },
     connection: {
-      host: 'localhost',
-      port: 15432,
-      user: 'plants_user',
-      password: 'password',
-      database: 'plants_db'
+      host: process.DB_HOST,
+      port: process.env.DB_PORT || 15432,
+      user: process.env.DB_USER || 'plants_user',
+      password: process.env.DB_PASSWORD || 'password',
+      database: process.env.DB_NAME || 'plants_db'
     },
+
+    // connection: 'postgres://plants_user:password@db/plants_db',
+
+    // connection: {
+    //   host: 'localhost',
+    //   port: 15432,
+    //   user: 'plants_user',
+    //   password: 'password',
+    //   database: 'plants_db'
+    // },
     pool: {
       min: 0,
       max: 10,
